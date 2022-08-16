83f56c49eef16cf91b3c711233690e10d7ccd2e1
Srijan945 <sainisrijan95820@gmail.com>
2022-08-16 15:52:59 +0530

    Done Basic Structuring

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(){
+    return <>          
+        <div class="footer">
+        Challenge by <a className='footer__a' href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a className='footer__a' href="#">Srijan Saini</a>.
+        </div>
+    </>
+}
+
+export default Footer;
