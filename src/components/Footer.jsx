432ae0fb2b4d49252f508d5c9bc3a35f2f0026ca
Srijan945 <sainisrijan95820@gmail.com>
2022-08-17 22:24:54 +0530

    Edit Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import '../css/Footer.css';
 function Footer(){
     return <>          
-        <div className="footer">
+        <footer className="footer">
         Challenge by <a className='footer__a' href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a className='footer__a' href="#">Srijan Saini</a>.
-        </div>
+        </footer>
     </>
 }
 
