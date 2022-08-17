b013ce738787a47dc7492617fa675776ecbd45ee
Srijan945 <sainisrijan95820@gmail.com>
2022-08-17 21:57:50 +0530

    Added Interactive Behaviour

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import '../css/Bar.css';
 
 function Bar({amount,day}){
-
+    const days=['sun','mon','tue','wed','thu','fri','sat'];
+    const [isHover,setIsHover]=useState(false);
+    const today = new Date().getDay();
     const value = 100*(amount/52.36);
     const styles= {
         height: `${value}px`,
-        backgroundColor: day ==='wed' ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)'
+        backgroundColor: day === days[today] ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)'
     }
+   
     return ( 
         <div className="bar">
-            <div className="bar__block" style={styles}></div>
+            <p className="bar__hover" style={{visibility: isHover ? 'visible' : 'hidden'}}>{`$${amount}`}</p>
+            <div 
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)} 
+            className="bar__block" 
+            style={styles}>
+            </div>
             <p className="bar__day">{day}</p>
         </div>
     ); 
